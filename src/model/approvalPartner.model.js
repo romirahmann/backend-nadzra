93ccd9a13e7f4a8802102989ca23df4f6808db91
romirahman <romirahman03romi@gmail.com>
2024-04-01 13:17:11 +0700

    last update

--- src/model/approvalPartner.model.js
+++ src/model/approvalPartner.model.js
@@ -14,11 +14,22 @@ getAll = async () =>
       "r.description",
       "r.nominal",
       "r.file_id as file_proof",
-      "r.status_code"
+      "r.status_code",
+      "u.username",
+      "u.karyawan_id",
+      "u.role_id",
+      "f.file_id",
+      "f.filename",
+      "ct.category_name",
+      "cl.client_name"
     )
     .from("approval_partner as ap")
     .join("approve_admin as a", "a.approval_admin_id", "ap.approval_admin_id")
-    .leftJoin("reimbursement as r", "r.claim_id", "a.claim_id");
+    .leftJoin("reimbursement as r", "r.claim_id", "a.claim_id")
+    .leftJoin("user as u", "u.user_id", "r.user_id")
+    .leftJoin("file_proof as f", "f.file_id", "r.file_id")
+    .leftJoin("categories as ct", "r.category_id", "ct.category_id")
+    .leftJoin("client as cl", "r.client_id", "cl.client_id");
 
 addApproval = async (data) => await db("approval_partner").insert(data);
 update = async (id, data) =>
--- src/model/client.model.js
+++ src/model/client.model.js
@@ -1,6 +1,6 @@
 const db = require("./../database/db.config");
 
-getAll = async () => await db.select("*").where("isDeleted", 0).from("client");
+getAll = async () => await db.select("*").where("is_deleted", 0).from("client");
 getById = async (id) => await db("client").where("client_id", id);
 insert = async (data) => await db("client").insert(data);
 update = async (id, data) =>
